bf88540bc5454ad36cf7179ed0da5b8d23b37e76
Dongsun Yoon <dsyoon@Dongsuns-MacBook-Pro.local>
2025-09-09 12:46:31 -0400

    add median

--- mid.py
+++ mid.py
@@ -1,8 +1,19 @@
+import sys
+import numpy as np
+
+def _median(arr):
+	print(np.median(arr))
+
+
 def _default():
 	print("IDK statistics")
 
 def main():
-	_default()
+	if len(sys.argv) == 1:
+		_default()
+	elif sys.argv[1] == 'median':
+		inarr = [float(x) for x in sys.argv[2:]]
+		_median(inarr)
 
 if __name__ == '__main__':
 	main()
