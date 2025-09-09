9ffcebaf447774fe788f366bfd8b6a0f8eb56a9b
Dongsun Yoon <dsyoon@Dongsuns-MacBook-Pro.local>
2025-09-09 12:41:46 -0400

    add average function

--- mid.py
+++ mid.py
@@ -1,8 +1,18 @@
+import sys
+import numpy as np
+
+def _avg(arr):
+	print(np.mean(arr))
+
 def _default():
 	print("IDK statistics")
 
 def main():
-	_default()
+	if len(sys.argv) == 1:
+		_default()
+	elif sys.argv[1] == 'mean':
+		inarr = [float(x) for x in sys.argv[2:]]
+		_avg(inarr)
 
 if __name__ == '__main__':
 	main()
